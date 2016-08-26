fbeb8cfd65c74f72a5eb12e53816af7f2b7a88a0
asep hidayat <asephidayat@aegis.co.id>
2016-08-26 14:38:30 +0700

    using default adapter

--- app/src/main/java/makers/asep/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/makers/asep/stormy/ui/DailyForecastActivity.java
@@ -3,6 +3,7 @@ package makers.asep.stormy.ui;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import makers.asep.stormy.R;
 
@@ -15,5 +16,11 @@ public class DailyForecastActivity extends ListActivity {
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday",
                 "Wednesday", "Thursday", "Friday", "Saturday" };
+        //array adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1,
+                daysOfTheWeek
+                );
+        setListAdapter(adapter);
     }
 }
