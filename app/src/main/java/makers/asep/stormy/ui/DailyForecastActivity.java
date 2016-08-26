a2fee251ef8708d6004c6f642ece3d8040a50543
asep hidayat <asephidayat@aegis.co.id>
2016-08-26 14:16:41 +0700

    introduction listView dan listActivity

---
+++ app/src/main/java/makers/asep/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,16 @@
+package makers.asep.stormy.ui;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import makers.asep.stormy.R;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+    }
+}
