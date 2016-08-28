fe5654226ab1f4a48cf52a503ba8d9b1b7f3422f
asep hidayat <asephidayat@aegis.co.id>
2016-08-28 22:11:59 +0700

    view holder

--- app/src/main/java/makers/asep/stormy/adapter/DayAdapter.java
+++ app/src/main/java/makers/asep/stormy/adapter/DayAdapter.java
@@ -1,10 +1,14 @@
 package makers.asep.stormy.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import makers.asep.stormy.R;
 import makers.asep.stormy.weather.Day;
 
 /**
@@ -38,10 +42,34 @@ public class DayAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            //jika belum ada view untuk ditampilkan
+            //create new view
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item,null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.iconImageView = (ImageView)convertView.findViewById(R.id.iconImageView);
+            viewHolder.temperatureLabel = (TextView)convertView.findViewById(R.id.temperatureLabel);
+            viewHolder.dayLabel = (TextView)convertView.findViewById(R.id.temperatureLabel);
+
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        //viewHolder.dayLabel.setText(mDays[position].ge);
+
+
         return null;
     }
 
     //class for reuse view
-
+    private static class ViewHolder{
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
 }
